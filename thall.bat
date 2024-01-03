1766ca5d84e4226faa465dd5e1295d5782a1f80f
Georg Veichtlbauer <georg@vware.at>
2024-01-03 20:37:33 +0100

    Tidy up the tidy batch.

--- thall.bat
+++ thall.bat
@@ -1,6 +1,4 @@
 @echo off
 for /f "tokens=*" %%a in ('dir /b /s *.htm') do tidy --doctype html5 --output-html yes --char-encoding utf8 --newline crlf --clean yes --indent auto -quiet --tidy-mark no --wrap 68 --write-back yes --add-meta-charset yes "%%a" | echo file: %%a
-echo .
-echo .
-dir *.html /o:d
+echo end.
 pause
